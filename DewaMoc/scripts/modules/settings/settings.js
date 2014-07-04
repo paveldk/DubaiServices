39db5144933a182b3158a5760d1ac906ae5b0cc7
Viktor Staikov <Viktor.staikov@telerik.com>
2014-07-04 04:37:19 -0500

    fix typos in settings.js and uuid.js file name

--- DewaMoc/scripts/modules/settings/settings.js
+++ DewaMoc/scripts/modules/settings/settings.js
@@ -45,11 +45,11 @@
 			that.initModule = $.proxy(that.initData, that);
 		},
             
-        _bindToEvents: {
+        _bindToEvents: function() {
             var that = this;
             
 			that.viewModel.on(that.viewModel.events.languageUpdate, $.proxy(that.setLanguage, that));
-        }    
+        },    
 
 		initData: function () {
 			var that = this;
